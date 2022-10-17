187cb25fea4df40831ba66844796f8ec35185b3f
matt53160 <matteodjerbi@hotmail.com>
2022-10-17 15:01:38 +0200

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -3,30 +3,29 @@ from pytube import YouTube
 from pytube import Playlist
 import os
   
-n=125
+counter=0
 p = Playlist('https://www.youtube.com/playlist?list=PLmPB89JPdlhZ87vPCrpT0B7D9RHEiQrdV')
-print(len(p.video_urls))
-for url in p.video_urls[126:463]:
+#print(len(p.video_urls))
+for url in p.video_urls[0:len(p.video_urls)]:
     
     # url input from user
-    yt = YouTube(
-        str(url))
+    yt = YouTube(str(url))
       
     # extract only audio
     video = yt.streams.filter(only_audio=True).first()
       
     # check for destination to save file
     
-    destination = "C:/Users/matte/Desktop/IA/HIP_HOP_MUSIC"
+    destination = "C:/Users/matte/Desktop/IA/HIP_HOP_MUSIC/"
       
     # download the file
     out_file = video.download(output_path=destination)
       
     # save the file
     base, ext = os.path.splitext(out_file)
-    new_file = "HIP_HOP_" + str(n) + '.wav'
-    os.rename(out_file, destination+ "/" + new_file)
+    new_file = "HIP_HOP_" + str(counter) + '.mp3'
+    os.rename(out_file, destination + new_file)
       
     # result of success
     print(new_file + " has been successfully downloaded.")
-    n=n+1
+    counter=counter+1
